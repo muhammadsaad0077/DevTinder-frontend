8b03649012fe33925b5b35970a0e3cc964f11bc4
muhammadsaad0077 <saaditanoli12@gmail.com>
2025-02-12 12:57:28 +0500

    making base url dynamic

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,5 @@
-export const base_url = "/api";
+// For production
+// export const base_url = "/api";
+
+// For dev
+  export const base_url = location.hostname === "localhost"? "http://localhost:3001": "/api";
